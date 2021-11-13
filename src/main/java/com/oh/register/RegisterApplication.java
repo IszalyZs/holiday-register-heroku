34fa72505a5f8712635075c37338e934e8389227
iszalyzs <iszalyzs@gmail.com>
2021-11-13 12:34:27 +0100

    improve project

--- src/main/java/com/oh/register/RegisterApplication.java
+++ src/main/java/com/oh/register/RegisterApplication.java
@@ -2,9 +2,10 @@ package com.oh.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RegisterApplication {
+public class RegisterApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterApplication.class, args);
